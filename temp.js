30cb91aecafbc3659156bafdfe557f5539330911
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2023-10-11 23:01:55 +0530

    first

---
+++ temp.js
@@ -0,0 +1,10 @@
+// console.log('Hello World')
+// const getJSON = async() => {
+//     const response = await fetch('convertcsv.json')
+//     const data = await response.json()
+//     return data
+// }
+
+// getJSON().then(data=>{
+
+// })
